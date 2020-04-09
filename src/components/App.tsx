bee45808d3e51d90e2f42a2d7e341e1dc5a17012
Meir Keller <mrzkeller@gmail.com>
2020-04-10 00:47:27 +0300

    locked orientation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Main from './Main';
 import Footer from './Footer';
 
-const App = () => (
-  <>
-    <Main />
-    <Footer />
-  </>
-);
+const App = () => {
+  useEffect(() => {
+    if (
+      'orientation' in window.screen &&
+      window.screen.orientation.type === 'landscape-primary'
+    ) {
+      window.screen.orientation.lock('landscape-primary');
+    }
+  }, []);
+
+  return (
+    <>
+      <Main />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
