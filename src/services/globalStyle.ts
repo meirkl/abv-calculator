aacc131f85d05f29807c3a7b08576673220eae76
Meir Keller <mrzkeller@gmail.com>
2020-04-12 13:34:01 +0300

    removed font files

--- src/services/globalStyle.ts
+++ src/services/globalStyle.ts
@@ -1,28 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 import { dark, primary } from '../constants/colors';
-import lobster from '../fonts/Lobster-Regular.ttf';
-import roboto from '../fonts/Roboto-Regular.ttf';
 
 export default createGlobalStyle`
-   @font-face {
-    font-family: 'Lobster';
-    src: url(${lobster});
-    font-style: normal;
-    font-weight: 400;
-  }
-  @font-face {
-    font-family: 'Roboto';
-    src: url(${roboto});
-    font-style: normal;
-    font-weight: 400;
-  }
   * {
-    box-sizing: border-box;
-  }
-  html,
-  body {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
   }
   body {
     font-family: 'Lobster', cursive;
@@ -32,5 +15,6 @@ export default createGlobalStyle`
   }
   h1 {
     font-size: 2.5rem;
+    margin: 20px 0;
   }
 `;
