1f5ebc0dae7a94479cfb13ca32e03cbd5d616eed
Meir Keller <mrzkeller@gmail.com>
2020-05-20 23:50:22 +0300

    added @types/react-gauge-chart

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,33 +1,5 @@
 /// <reference types="react-scripts" />
 
-declare module 'react-gauge-chart' {
-  import React from 'react';
-
-  type Props = {
-    id: string;
-    className?: string;
-    style?: React.CSSProperties;
-    marginInPercent?: number;
-    cornerRadius?: number;
-    nrOfLevels?: number;
-    percent?: number;
-    arcPadding?: number;
-    arcWidth?: number;
-    arcsLength?: number[];
-    colors?: string[];
-    textColor?: string;
-    needleColor?: string;
-    needleBaseColor?: string;
-    hideText?: boolean;
-    animate?: boolean;
-    animDelay?: number;
-    formatTextValue?: (value: string) => string;
-    fontSize?: string;
-  };
-
-  export default function GaugeChart(props: Props): React.ReactElement;
-}
-
 declare module 'react-context-devtool' {
   import React from 'react';
 
