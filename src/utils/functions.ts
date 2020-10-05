b763df16c69a2c9766c4f7b602ce11101534d690
Meir Keller <mrzkeller@gmail.com>
2020-10-06 01:18:22 +0300

    Update functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -51,7 +51,6 @@ export function SGToBrix(sg: number) {
   return ((182.4601 * sg - 775.6821) * sg + 1262.7794) * sg - 669.5622;
 }
 
-//
 /**
  * formula source: https://www.homebrewtalk.com/threads/temp-correction-formula-for-hydrometer.10684/
  *
